2517df7d6ebdc0359406167f8b138b872aeae164
Joachim Stolberg <joe.stolberg@gmx.de>
2017-10-08 17:53:37 +0200

    v0.04 version adapted

--- init.lua
+++ init.lua
@@ -12,12 +12,17 @@
 	2017-09-08  v0.01  first version
 	2017-09-17  v0.02  harvester added
 	2017-10-02  v0.03  fermenter and reformer added
+	2017-10-08  v0.04  Mesecons Converter and Programmer Tool added
 
 ]]--
 
-dofile(minetest.get_modpath("tubelib_addons2") .. "/timer.lua")
-dofile(minetest.get_modpath("tubelib_addons2") .. "/repeater.lua")
-dofile(minetest.get_modpath("tubelib_addons2") .. "/programmer.lua")
-if mesecon then
-	dofile(minetest.get_modpath("tubelib_addons2") .. "/mesecons_converter.lua")
+if tubelib.version >= 0.06 then
+	dofile(minetest.get_modpath("tubelib_addons2") .. "/timer.lua")
+	dofile(minetest.get_modpath("tubelib_addons2") .. "/repeater.lua")
+	dofile(minetest.get_modpath("tubelib_addons2") .. "/programmer.lua")
+	if mesecon then
+		dofile(minetest.get_modpath("tubelib_addons2") .. "/mesecons_converter.lua")
+	end
+else
+	print("[tubelib_addons2] Version 0.06+ of Tubelib Mod is required!")
 end
